d3d72348e83dc46111ad8c49b2f00f45294df499
Michael Trew <mtrew2015@gmail.com>
2023-07-25 14:53:47 -0400

    refactoring using workers

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,89 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { CirclePicker } from 'react-color';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import Worker from 'worker-loader!./timeWorker.js';
 
 import './App.scss';
 
 function App() {
-	const [ time, setTime ] = useState(1500);
-	const [ active, setActive ] = useState(false);
-	const [ count, setCount ] = useState(0);
-	const [ onBreak, setOnBreak ] = useState(false);
-	const [ bgColor, setbgColor ] = useState('#fff');
+  const [time, setTime] = useState(1500);
+  const [active, setActive] = useState(false);
+  const [count, setCount] = useState(0);
+  const [onBreak, setOnBreak] = useState(false);
+  const [bgColor, setBgColor] = useState('#fff');
 
-	const audio = new Audio('/bell.mp3');
+  const workerRef = useRef(null);
 
-	useEffect(
-		() => {
-			const shortOrLong = () => {
-				if (count % 2 != 0) {
-					return 600;
-				} else {
-					return 300;
-				}
-			};
-			let int = null;
-			document.title = onBreak ? `Break: ${formatTime()}` : `Work: ${formatTime()}`;
-			if (active && time > 0) {
-				int = setInterval(() => {
-					setTime((time) => time - 1);
-				}, 1000);
-			} else {
-				if (active && time === 0 && !onBreak) {
-					setActive(false);
-					setTime(shortOrLong());
-					setOnBreak(true);
-					setCount(count + 1);
-					audio.play();
-				} else if (onBreak && time === 0) {
-					audio.play();
-					setOnBreak(false);
-					setTime(1500);
-					clearInterval(int);
-					setActive(false);
-				}
-			}
-			return () => clearInterval(int);
-		},
-		[ onBreak, time, active, count, audio ]
-	);
+  useEffect(() => {
+    // Create the worker when the component mounts
+    if (!workerRef.current) {
+      workerRef.current = new Worker();
+    }
+	console.log('workerRef.current', workerRef.current);
 
-	const formatTime = () => {
-		let minutes = Math.floor(time / 60);
-		let seconds = time % 60;
-		return `${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
-	};
+    if (active) {
+      workerRef.current.onmessage = (event) => {
+		console.log(event.data);
+        setTime(event.data.time);
+        document.title = event.data.formatTime;
+      };
+      workerRef.current.postMessage({ command: 'start', time, onBreak });
+    } else if (!active) {
+      console.log('stopping');
+      workerRef.current.postMessage({ command: 'stop' });
+	  workerRef.current.terminate();
+	  workerRef.current = null;
+    }
 
-	const toggleColor = () => {
-		const color = document.getElementById('colorPicker');
-		color.classList.toggle('colorPicker');
-	};
+    // Terminate the worker when the component unmounts
+    return () => {
+      if (workerRef.current) {
+        workerRef.current.terminate();
+      }
+    };
+  }, [active]); // Removed `time` from the dependency array
 
-	const handleColorChange = (color) => {
-		const wrapper = document.getElementById('container');
-		console.log('clicked in color');
-		setbgColor(color);
-		toggleColor();
-		wrapper.style.backgroundColor = color.hex;
-	};
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  };
 
-	return (
-		<div className="App" id="container">
-			<h1>Pomodoro Timer</h1>
-			<h2>
-				{onBreak ? 'Break: ' : 'Work: '}
-				{formatTime()}
-			</h2>
-			<p>{`Pomodoros Completed: ${count}`}</p>
-			<div className="button-row">
-				<button onClick={() => toggleColor()}>Change Color</button>
+  const toggleColor = () => {
+    const colorPicker = document.getElementById('colorPicker');
+    colorPicker.classList.toggle('colorPicker');
+  };
 
-				<button onClick={() => setActive(!active)}>{active ? 'Pause' : 'Start'}</button>
-			</div>
-			<div id="colorPicker" className="colorPicker">
-				<CirclePicker color={bgColor} onChange={(color, e) => handleColorChange(color, e)} />
-			</div>
-		</div>
-	);
+  const handleColorChange = (color) => {
+    const wrapper = document.getElementById('container');
+    setBgColor(color.hex);
+    toggleColor();
+    wrapper.style.backgroundColor = color.hex;
+  };
+
+  return (
+    <div className='App' id='container'>
+      <h1>Pomodoro Timer</h1>
+      <h2>
+        {onBreak ? 'Break: ' : 'Work: '}
+        {formatTime()}
+      </h2>
+      <p>{`Pomodoros Completed: ${count}`}</p>
+      <div className='button-row'>
+        <button onClick={() => toggleColor()}>Change Color</button>
+        <button onClick={() => setActive(!active)}>
+          {active ? 'Pause' : 'Start'}
+        </button>
+      </div>
+      <div id='colorPicker' className='colorPicker'>
+        <CirclePicker
+          color={bgColor}
+          onChange={(color) => handleColorChange(color)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
